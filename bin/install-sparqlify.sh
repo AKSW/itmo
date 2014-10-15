de7493ee523e38ff36328fcb5391e6bd3ea137f3
Ivan Ermilov <earthquakesan@gmail.com>
2014-10-15 10:33:26 +0400

    install script update

--- bin/install-sparqlify.sh
+++ bin/install-sparqlify.sh
@@ -3,3 +3,4 @@
 REPOSITORY=$(dirname $PWD)
 
 echo "java -Duser.country=ru -Duser.language=ru -cp \"$REPOSITORY/lib/ojdbc6.jar:$REPOSITORY/lib/sparqlify-core-0.6.13-SNAPSHOT-jar-with-dependencies.jar\" org.aksw.sparqlify.web.Main \"\$@\"" > /usr/bin/sparqlify
+chmod +x /usr/bin/sparqlify
