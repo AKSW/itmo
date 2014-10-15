42e5327db1839a14f19770e6c980fa3b7434b89c
Ivan Ermilov <earthquakesan@gmail.com>
2014-10-15 11:54:33 +0400

    sparqlify.sh saving data

--- bin/sparqlify.sh
+++ bin/sparqlify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sparqlify -D -U ISU_SEMANTIC -W fltu1632 -j jdbc:oracle:thin:ISU_SEMANTIC/fltu1632@192.168.1.2:1521/oracis -m ../mappings/$1/$2.sml
+sparqlify -D -U ISU_SEMANTIC -W fltu1632 -j jdbc:oracle:thin:ISU_SEMANTIC/fltu1632@192.168.1.2:1521/oracis -m ../mappings/$1/$2.sml | tee ../data/$2.nt
