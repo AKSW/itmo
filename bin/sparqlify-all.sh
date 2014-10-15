b36ab772b33e1e993781a5ca6cfb646d2dc75d80
Ivan Ermilov <earthquakesan@gmail.com>
2014-10-15 11:34:58 +0400

    change sparqlify-all script for smaller footprint

--- bin/sparqlify-all.sh
+++ bin/sparqlify-all.sh
@@ -5,6 +5,11 @@ ROOT_DIR=$(dirname $CURRENT_DIR)
 rm $ROOT_DIR/mappings/all.sml
 for D in `find $ROOT_DIR/mappings -type d`
 do
-  for file in $D/*.sml; do cat $file; done >> $ROOT_DIR/mappings/all.sml
+  for file in $D/*.sml; 
+  do 
+    filename=$(basename "$file")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    sparqlify -D -U ISU_SEMANTIC -W fltu1632 -j jdbc:oracle:thin:ISU_SEMANTIC/fltu1632@192.168.1.2:1521/oracis -m $file | tee $ROOT_DIR/data/$filename.nt
+  done 
 done
-sparqlify -D -U ISU_SEMANTIC -W fltu1632 -j jdbc:oracle:thin:ISU_SEMANTIC/fltu1632@192.168.1.2:1521/oracis -m $ROOT_DIR/mappings/all.sml | tee $ROOT_DIR/data/itmo.nt
