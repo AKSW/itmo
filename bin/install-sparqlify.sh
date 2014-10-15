0fdb934d951e339e15af4a98943e3cb628283026
Ivan Ermilov <earthquakesan@gmail.com>
2014-10-15 10:26:50 +0400

    fixed localization in sparqlify - run install-sparqlify.sh again

--- bin/install-sparqlify.sh
+++ bin/install-sparqlify.sh
@@ -2,4 +2,4 @@
 
 REPOSITORY=$(dirname $PWD)
 
-echo "java -cp \"$REPOSITORY/lib/ojdbc6.jar:$REPOSITORY/lib/sparqlify-core-0.6.13-SNAPSHOT-jar-with-dependencies.jar\" org.aksw.sparqlify.web.Main \"\$@\"" > /usr/bin/sparqlify
+echo "java -Duser.country=ru -Duser.language=ru -cp \"$REPOSITORY/lib/ojdbc6.jar:$REPOSITORY/lib/sparqlify-core-0.6.13-SNAPSHOT-jar-with-dependencies.jar\" org.aksw.sparqlify.web.Main \"\$@\"" > /usr/bin/sparqlify
