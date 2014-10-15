59d24f72c1359a3cac7781989c50f93edb0bff1d
Ivan Ermilov <earthquakesan@gmail.com>
2014-10-15 10:44:17 +0400

    java heap max setup for sparqlify

--- bin/install-sparqlify.sh
+++ bin/install-sparqlify.sh
@@ -2,5 +2,5 @@
 
 REPOSITORY=$(dirname $PWD)
 
-echo "java -Duser.country=ru -Duser.language=ru -cp \"$REPOSITORY/lib/ojdbc6.jar:$REPOSITORY/lib/sparqlify-core-0.6.13-SNAPSHOT-jar-with-dependencies.jar\" org.aksw.sparqlify.web.Main \"\$@\"" > /usr/bin/sparqlify
+echo "java -Xmx4196m -Duser.country=ru -Duser.language=ru -cp \"$REPOSITORY/lib/ojdbc6.jar:$REPOSITORY/lib/sparqlify-core-0.6.13-SNAPSHOT-jar-with-dependencies.jar\" org.aksw.sparqlify.web.Main \"\$@\"" > /usr/bin/sparqlify
 chmod +x /usr/bin/sparqlify
