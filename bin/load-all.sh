f22deadae22f8754c44529f47d507244cbb9eaaf
Ivan Ermilov <earthquakesan@gmail.com>
2014-10-16 11:48:52 +0400

    virtdelete and check on load

--- bin/load-all.sh
+++ bin/load-all.sh
@@ -6,9 +6,24 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DATA_DIR=$(dirname $(pwd))/data
 DATA_FILE=$DATA_DIR/itmo.nt
+ERROR=0
 for file in $DATA_DIR/*.nt; 
 do
-  cat $file;
+  FILESIZE=$(stat -c%s $file)
+  if [ $FILESIZE -eq 0 ]; 
+  then
+    echo "$file got errors" | tee -a errors.txt
+    $ERROR=1
+  else
+    echo "$file is okay"
+    cat $file;
+  fi;
 done 2>/dev/null >> $DATA_DIR/itmo.nt
 #load into virtuoso
-virtload $DATA_FILE http://lod.ifmo.ru
+if [ $ERROR -eq 0 ];
+then
+  echo "loading to the server"
+  #virtload $DATA_FILE http://lod.ifmo.ru
+else
+  echo "Some of the files are not converted correctly (contains no data)"
+fi;
