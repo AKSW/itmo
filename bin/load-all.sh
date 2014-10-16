66851fba7ebc24c1b7ab99c0a088008dfc9a8c88
Konrad Höffner <konrad@gmx.at>
2014-10-16 11:38:21 +0400

    sparqlify script now deletes old file before conversion.

--- bin/load-all.sh
+++ bin/load-all.sh
@@ -2,10 +2,13 @@
 
 #transform the data
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$CURRENT_DIR/sparqlify-all.sh
+#$CURRENT_DIR/sparqlify-all.sh
 
 DATA_DIR=$(dirname $(pwd))/data
 DATA_FILE=$DATA_DIR/itmo.nt
-echo $DATA_FILE
+for file in $DATA_DIR/*.nt; 
+do
+  cat $file;
+done 2>/dev/null >> $DATA_DIR/itmo.nt
 #load into virtuoso
 virtload $DATA_FILE http://lod.ifmo.ru
--- bin/sparqlify-direct.sh
+++ bin/sparqlify-direct.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sparqlify -D -U ISU_SEMANTIC -W fltu1632 -j jdbc:oracle:thin:ISU_SEMANTIC/fltu1632@194.85.163.21:1521/oracis -m ../mappings/$1.sml
+#sparqlify -D -U ISU_SEMANTIC -W fltu1632 -j jdbc:oracle:thin:ISU_SEMANTIC/fltu1632@192.168.134.3:1521/oracis -m ../mappings/$1/$2.sml
+sparqlify -D -U ISU_SEMANTIC -W fltu1632 -j jdbc:oracle:thin:ISU_SEMANTIC/fltu1632@194.85.163.158:1521/oracis -m ../mappings/$1/$2.sml
