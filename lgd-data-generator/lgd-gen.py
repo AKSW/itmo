dbb58df33a23535e0655291e14aaa3a7d3125647
Ivan Ermilov <earthquakesan@gmail.com>
2015-03-16 01:07:39 +0100

    extended with iso codes

--- lgd-data-generator/lgd-gen.py
+++ lgd-data-generator/lgd-gen.py
@@ -8,9 +8,13 @@ import codecs
 outputFile = codecs.open("lgdLabels.nt", "w", "utf-8")
 lgdList = list(set([line.rstrip() for line in open('lgd.list')]))
 rdfsLabel = "http://www.w3.org/2000/01/rdf-schema#label"
+countryCodePropertyFips = "http://linkedgeodata.org/ontology/country_code_fips"
+countryCodePropertyIso = "http://linkedgeodata.org/ontology/country_code_iso3166_1_alpha_2"
+countryCodeProperty = "http://linkedgeodata.org/ontology/country_code_iso3166"
 
 sparql = SPARQLWrapper("http://linkedgeodata.org/sparql")
 for lgdEntity in lgdList:
+    print lgdEntity
     sparql.setQuery("""
         SELECT ?label_en
         WHERE {
@@ -32,8 +36,24 @@ for lgdEntity in lgdList:
     """ % (lgdEntity,))
     results = sparql.query().convert()
     labelRu = results['results']['bindings'][0]['label_ru']['value']
+
+    sparql.setQuery("""
+        SELECT ?country_code_fips ?country_code_iso
+        WHERE {
+            <%s> ?p ?o .
+            OPTIONAL {<%s> <%s> ?country_code_fips .}
+            OPTIONAL {<%s> <%s> ?country_code_iso .}
+        }
+    """ % (lgdEntity, lgdEntity, countryCodePropertyFips, lgdEntity, countryCodePropertyIso))
+    results = sparql.query().convert()
+    try: 
+        countryCode = results['results']['bindings'][0]['country_code_iso']['value']
+    except:
+        countryCode = results['results']['bindings'][0]['country_code_fips']['value']
+
     outputFile.write(u'<%s> <%s> "%s"@en .\n' % (lgdEntity, rdfsLabel, labelEn))
     outputFile.write(u'<%s> <%s> "%s"@ru .\n' % (lgdEntity, rdfsLabel, labelRu))
+    outputFile.write(u'<%s> <%s> "%s" .\n' % (lgdEntity, countryCodeProperty, countryCode))
 
 #For DBpedia needs another endpoint
 dbpediaEntity = "http://dbpedia.org/resource/Portugal"
@@ -60,7 +80,9 @@ sparql.setQuery("""
 """ % (dbpediaEntity,))
 results = sparql.query().convert()
 labelRu = results['results']['bindings'][0]['label_ru']['value']
+
 outputFile.write(u'<%s> <%s> "%s"@en .\n' % (dbpediaEntity, rdfsLabel, labelEn))
 outputFile.write(u'<%s> <%s> "%s"@ru .\n' % (dbpediaEntity, rdfsLabel, labelRu))
+outputFile.write(u'<%s> <%s> "PT" .\n' % (lgdEntity, countryCodeProperty))
 
 outputFile.close()
